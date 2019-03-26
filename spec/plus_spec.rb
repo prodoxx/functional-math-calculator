91dc0191c746515ac6e9abd2f7c5e7d764d75f49
Reggie Gian Escobar <me@reggieescobar.com>
2019-03-26 17:24:25 -0600

    Added more tests for the Plus Operation

--- spec/plus_spec.rb
+++ spec/plus_spec.rb
@@ -9,4 +9,20 @@ RSpec.describe 'Test Plus Operation Handling' do
   it 'SAD: should add two and five incorrectly' do
     expect(two(plus(five))).not_to eql 4
   end
+
+  it 'HAPPY: should add one and nine correctly' do
+    expect(one(plus(five)))to eq 10
+  end
+
+  it 'SAD: should add five and  eight correctly' do
+    expect(five(plus(eight))).not_to eq 13
+  end
+
+  it 'HAPPY: should add seven and six correctly' do
+    expect(seven(plus(six))).to eq 13
+  end
+
+  it 'HAPPY: should add four and three' do
+    expect(four(plus(three))).to eq 7
+  end
 end
