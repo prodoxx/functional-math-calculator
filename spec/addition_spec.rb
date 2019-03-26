61dcb16be5eb16e84dd3aeec451505cdaf55bfa9
Reggie Gian Escobar <me@reggieescobar.com>
2019-03-26 11:06:16 -0500

    Added to addition tests to test for an correct and incorrect value

---
+++ spec/addition_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../math_calculator.rb'
+
+RSpec.describe 'Math Calculator', 'Test Addition Handling' do
+  it 'HAPPY: should add one and one correctly' do
+    expect(one(plus(one))).to eq 2
+  end
+
+  it 'SAD: should add two and five incorrectly' do
+    expect(two(plus(five))).not_to eql 4
+  end
+end
