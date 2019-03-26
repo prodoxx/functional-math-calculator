b9c78837079bd0ecc1b86f35f1fc0c53ed05a12d
Reggie Gian Escobar <me@reggieescobar.com>
2019-03-26 17:39:44 -0600

    Added more minus operation tests; all minus operation tests passed

--- spec/minus_spec.rb
+++ spec/minus_spec.rb
@@ -11,4 +11,24 @@ RSpec.describe 'Test Minus Operation Handling' do
   it 'SAD: should minus five and three incorrectly' do
     expect(five(minus(three))).not_to eql 1
   end
+
+  it 'HAPPY: should minus nine and five correctly' do
+    expect(nine(minus(five))).to eql 4
+  end
+
+  it 'HAPPY: should minus four and two correctly' do
+    expect(four(minus(two))).to eql 2
+  end
+
+  it 'HAPPY: should minus seven and six correctly' do
+    expect(seven(minus(six))).to eql 1
+  end
+
+  it 'HAPPY: should minus four and nine correctly' do
+    expect(four(minus(nine))).to eql -5
+  end
+
+  it 'HAPPY: should minus six minus three correctly' do
+    expect(six(minus(three))).to eql 3
+  end
 end
