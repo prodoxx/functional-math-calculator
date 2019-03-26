3953b1e74b71ed3a2db2c94d33f95c6a02ff7f83
Reggie Gian Escobar <me@reggieescobar.com>
2019-03-26 16:38:03 -0600

    Added the minus function; tested the plus and minus function - both test sets passed for existing specs

--- operations.rb
+++ operations.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: false
 
-
 def plus(n)
   plus_func = ->(a, b) { a + b }
+  plus_func.curry.call(n)
+end
+
+def minus(n)
+  plus_func = ->(a, b) { b - a }
   plus_func.curry.call(n)
 end
 
