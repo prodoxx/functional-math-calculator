c4af07b9809bc9e6f79b8d24b485fc109bef8652
Reggie Gian Escobar <me@reggieescobar.com>
2019-03-26 18:27:01 -0600

    Added more Times tests for mostly all possible number combinations; all tests passed

--- spec/times_spec.rb
+++ spec/times_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe 'Test Times Operation Handling' do
   it 'SAD: should times eight and two incorrectly' do
     expect(five(minus(three))).not_to eql 3
   end
+
+  it 'HAPPY: should times five and nine correctly' do
+    expect(five(times(nine))).to eql 45
+  end
+
+  it 'HAPPY: should times three and seven correctly' do
+    expect(three(times(seven))).to eql 21
+  end
+
+  it 'HAPPY: should times one and zero correctly' do
+    expect(one(times(zero))).to eql 0
+  end
+
+  it 'HAPPY: should times one and eight correctly' do
+    expect(one(times(eight))).to eql 8
+  end
 end
