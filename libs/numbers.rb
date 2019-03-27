9646b9265dd823b874bd70bd94ee45ad03847c50
Reggie Gian Escobar <me@reggieescobar.com>
2019-03-26 21:59:30 -0600

    Made the operations arguments more understandable to follow the requirements

--- libs/numbers.rb
+++ libs/numbers.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-
 def default(number)
   number
 end
--- libs/operations.rb
+++ libs/operations.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 def plus(number)
-  plus_func = ->(left_operand, right_operand) { right_operand + left_operand }
+  plus_func = ->(right_operand, left_operand) { left_operand + right_operand }
   plus_func.curry.call(number)
 end
 
 def minus(number)
-  minus_func = ->(left_operand, right_operand) { right_operand - left_operand }
+  minus_func = ->(right_operand, left_operand) { left_operand - right_operand }
   minus_func.curry.call(number)
 end
 
 def times(number)
-  times_func = ->(left_operand, right_operand) { right_operand * left_operand }
+  times_func = ->(right_operand, left_operand) { left_operand * right_operand }
   times_func.curry.call(number)
 end
 
 def divided_by(number)
-  divide_func = ->(left_operand, right_operand) { right_operand / left_operand }
+  divide_func = ->(right_operand, left_operand) { left_operand / right_operand }
   divide_func.curry.call(number)
 end
